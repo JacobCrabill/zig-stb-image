2e918a24697eefc9c401b7e9bd894c55c9d0ec7f
Jacob Crabill <jcrabill@darkhive.com>
2025-04-29 15:42:35 -0700

    Cleanup build.zig

--- build.zig
+++ build.zig
@@ -4,36 +4,13 @@ const std = @import("std");
 const CFlags = &[_][]const u8{"-fPIC"};
 
 pub fn build(b: *std.Build) !void {
-    comptime {
-        const current_zig = builtin.zig_version;
-        const min_zig = std.SemanticVersion.parse("0.12.0-dev.2030") catch unreachable; // build system changes: ziglang/zig#18160
-        if (current_zig.order(min_zig) == .lt) {
-            @compileError(std.fmt.comptimePrint("Your Zig version v{} does not meet the minimum build requirement of v{}", .{
-                current_zig,
-                min_zig,
-            }));
-        }
-    }
-
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
+    const linkage = b.option(std.builtin.LinkMode, "linkage", "Static or dynamic linkage") orelse .static;
 
     ////////////////////////////////////////////////////////////////////////////
     // Create the Zig STB Image Module
     ////////////////////////////////////////////////////////////////////////////
-
-    // Compile the C source file into a static library, and ensure it gets installed
-    // along with the required include directory
-    const stb = b.addStaticLibrary(.{
-        .name = "stb-image",
-        .optimize = optimize,
-        .target = target,
-        .link_libc = true,
-    });
-    stb.addIncludePath(b.path("include"));
-    stb.addCSourceFile(.{ .file = b.path("src/stb_image.c"), .flags = CFlags });
-    stb.installHeadersDirectory(b.path("include/stb"), "stb", .{});
-    b.installArtifact(stb);
 
     // Export the 'stb_image' module to downstream packages
     //
@@ -42,9 +19,22 @@ pub fn build(b: *std.Build) !void {
     // should "Just Work"
     const mod = b.addModule("stb_image", .{
         .root_source_file = b.path("src/stb_image.zig"),
+        .target = target,
+        .optimize = optimize,
         .link_libc = true,
     });
-    mod.linkLibrary(stb);
+    mod.addIncludePath(b.path("include"));
+    mod.addCSourceFile(.{ .file = b.path("src/stb_image.c"), .flags = CFlags });
+
+    // Compile the C source file into a library, and ensure it gets installed
+    // along with the required include directory
+    const stb = b.addLibrary(.{
+        .name = "stb-image",
+        .root_module = mod,
+        .linkage = linkage,
+    });
+    stb.installHeadersDirectory(b.path("include"), "", .{});
+    b.installArtifact(stb);
 
     ////////////////////////////////////////////////////////////////////////////
     // Example application using zig-stb-image
